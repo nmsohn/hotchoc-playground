132f7378f8a320c7ce8552fedf78a78cc4fa543c
nmsohn <odna425@gmail.com>
2022-01-17 18:24:23 +1300

    Added UI

---
+++ DemoUI/DataAccess/Model/CreateEmployeeResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoUI.DataAccess.Model
+{
+    public class CreateEmployeeResponse
+    {
+        public int DepartmentId { get; set; }
+
+        public string Name { get; set; }
+
+        public int EmployeeId { get; set; }
+    }
+}
